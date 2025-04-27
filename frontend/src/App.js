83842757925d0a77323baa81cd6a2ce419e29d03
unknown <hackerviruse04@gmail.com>
2025-04-27 15:09:46 +0530

    added smooth scrolling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -7,34 +7,21 @@ import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/Sign-up';
 
+import { ReactLenis } from 'lenis/react';  
+
 function App() {
-
   return (
-
     <div className="App">
-           
-      <Router>
-        <Routes>
-          <Route exact
-            path="/"
-            element={<Home />}
-          ></Route>
-          <Route exact
-            path="/services"
-            element={<Services />}
-          ></Route>
-          <Route exact
-            path="/Products"
-            element={<Products />}
-          ></Route>
-          <Route exact
-            path="/Sign-up"
-            element={<SignUp />}
-          ></Route>
-        </Routes>
-
-      </Router>
-
+      <ReactLenis root> {/* 👈 wrap everything inside ReactLenis */}
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/services" element={<Services />} />
+            <Route exact path="/Products" element={<Products />} />
+            <Route exact path="/Sign-up" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </ReactLenis>
     </div>
   );
 }
